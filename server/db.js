2e43482681cfd11dade80125f4396d5845cb6e5a
Chandrima Hazra <hazrachandrima6@gmail.com>
2026-04-15 09:48:56 +0530

    added dockerfile and docker-compose.yml

--- server/db.js
+++ server/db.js
@@ -1,12 +1,10 @@
 const mysql = require("mysql2/promise");
-
 const db = mysql.createPool({
-  host: "localhost",
+  host: process.env.DB_HOST || "localhost",  // ← only change
   user: "root",
   password: process.env.DB_PASSWORD,
   database: "todo_app",
   waitForConnections: true,
   connectionLimit: 10
 });
-
 module.exports = db;
